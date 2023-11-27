4ba6d1676710aae653363692cdfd507fd149bdd4
Alexander Zakidyshev <alexander.zakidishev@tern.ru>
2023-11-27 20:14:01 +0300

    25 - 25 Executable jar.mp4

---
+++ service/src/main/java/ru/zalex/service/AppRunner.java
@@ -0,0 +1,12 @@
+package ru.zalex.service;
+
+import ru.zalex.service.service.UserService;
+
+public class AppRunner {
+
+  public static void main(String[] args) {
+    var userService = new UserService();
+    System.out.println(userService.getUser(25L));
+  }
+
+}
